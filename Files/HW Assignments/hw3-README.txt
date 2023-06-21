ebc4b31684d3264ae7fe0476e25bb7b4c5c915c4
gandharkulkarni <gakulkarni@dons.usfca.edu>
2023-06-20 18:48:35 -0700

    HW3 complete

---
+++ Files/HW Assignments/hw3-README.txt
@@ -0,0 +1,26 @@
+Name: Gandhar Kulkarni
+Student ID: 20702101
+
+Homework Description:
+
+This homework assignment focuses on if statements, loops and Processing table data using 2-dimensional arrays in JavaScript.
+
+The sales array represents sales data for months Jan – Dec for the years 2016 – 2018. Each row
+represents a year, and each column represents the month.
+
+Part 1:
+fCalcRowTotal() : Calculates and returns total sales of the year (row) in 2D array data. This function takes 2 parameters - 2D array and rowIndex.
+
+Part 2:
+fCalcColTotal() : Calculates and returns total sales of the month (column) for all years in 2D array data. This function takes 2 parameters - 2D array and colIndex.
+
+Part 3:
+fCalcTotals() : Calculates and prints total sales per year and total sales per month in 2D array data. This function takes 1 parameters - 2D array.
+
+Part 4:
+fCalcAverageTotals() : Calculates and prints total sales per year, average sales per year, total sales per month, average sales for the month in 2D array data. This function takes 1 parameters - 2D array.
+
+Problems faced: None
+Resources used: Lecture slides
+
+
--- Files/HW Assignments/hw3.js
+++ Files/HW Assignments/hw3.js
@@ -1,12 +1,13 @@
+//2D  array below represents sales data for months Jan – Dec for the years 2016 – 2018. Each row represents a year, and each column represents the month.
 let sales = [
     [345.54, 389.21, 745.90, 451.89, 395.67, 451.45, 598.42, 575.64, 523.78, 678.55, 874.56, 945.34],
     [423.54, 423.87, 432.87, 534.77, 385.29, 642.89, 452.84, 523.88, 554.78, 767.90, 523.44, 899.34],
     [623.87, 518.66, 372.67, 745.56, 634.56, 429.88, 528.67, 418.56, 276.52, 514.95, 639.52, 786.58]
 ];
-// console.log(fCalcRowTotal(sales,1));
-// console.log(fCalcColTotal(sales,12));
-// fCalcTotals(sales);
-fCalcAverageTotals(sales);
+console.log(fCalcRowTotal(sales,1));
+console.log(fCalcColTotal(sales,5));
+fCalcTotalSales(sales);
+fCalcTotalSalesWithAvg(sales);
 
 // Part 1
 //Function to calculate total sales for a year
@@ -15,7 +16,7 @@ function fCalcRowTotal(salesArr, rowIndex){
         return "Invalid Row Index";
     }else{
         let rowTotal=0;
-        for(let i=0; i<12; i++){
+        for(let i=0; i<salesArr[rowIndex].length; i++){
             rowTotal += salesArr[rowIndex][i];
         }
         return rowTotal
@@ -31,7 +32,7 @@ function fCalcColTotal(salesArr, colIndex){
     }
     else{
         let colTotal =0;
-        for(let i=0; i<3; i++){
+        for(let i=0; i<salesArr.length; i++){
             colTotal += salesArr[i][colIndex];
         }
         return colTotal;
@@ -40,17 +41,17 @@ function fCalcColTotal(salesArr, colIndex){
 
 // Part 3
 // Function to calculate yearly and monthly total sales
-function fCalcTotals(salesArr){
+function fCalcTotalSales(salesArr){
     let year = 2016;
     let month = 1;
     console.log("------------Totals per year------------");
-    for(let i=0; i<3; i++){
+    for(let i=0; i<salesArr.length; i++){
         let yearlyTotal = fCalcRowTotal(sales,i);
         console.log(`Total for year ${year}\t\t= ${yearlyTotal.toFixed(2)}`);
         year++;
     }
     console.log("------------Totals per Month------------");
-    for(let i=0; i<12; i++){
+    for(let i=0; i<salesArr[0].length; i++){
         let monthlyTotal = fCalcColTotal(sales, i);
         console.log(`Total for month ${month}\t\t= ${monthlyTotal.toFixed(2)}`);
         month++;
@@ -59,23 +60,23 @@ function fCalcTotals(salesArr){
 
 // Part 4
 // Function to calculate yearly and monthly average sales
-function fCalcAverageTotals(salesArr){
+function fCalcTotalSalesWithAvg(salesArr){
     let year = 2016;
     let month = 1;
     console.log("------------Totals per year------------");
     let sum=0;
-    for(let i=0; i<3; i++){
+    for(let i=0; i<salesArr.length; i++){
         let yearlyTotal = fCalcRowTotal(sales,i);
         sum +=yearlyTotal;
         console.log(`Total for year ${year}\t\t= $${yearlyTotal.toFixed(2)}`);
         year++;
     }
     console.log("------------------------");
-    console.log(`Yearly average sales\t\t= $${sum/3}`);
+    console.log(`Yearly average sales\t\t= $${sum/salesArr.length}`);
     console.log("------------Totals per Month------------");
-    for(let i=0; i<12; i++){
+    for(let i=0; i<salesArr[0].length; i++){
         let monthlyTotal = fCalcColTotal(sales, i);
-        console.log(`Total for month ${month}\t\t= $${monthlyTotal.toFixed(2)} (Monthly average sales = $${(monthlyTotal/3).toFixed(2)})`);
+        console.log(`Total for month ${month}\t\t= $${monthlyTotal.toFixed(2)} (Monthly average sales = $${(monthlyTotal/salesArr.length).toFixed(2)})`);
         month++;
     }
 }
