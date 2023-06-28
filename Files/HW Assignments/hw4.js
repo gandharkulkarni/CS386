6b1d25ab259643ab9401023e8c04eee8c9c4872e
gandharkulkarni <gakulkarni@dons.usfca.edu>
2023-06-27 19:00:10 -0700

    Added comments

--- Files/HW Assignments/hw4.js
+++ Files/HW Assignments/hw4.js
@@ -1,16 +1,24 @@
 window.addEventListener("DOMContentLoaded",fLoad);
 
-function fLoad(){
-  let loginButton = document.getElementById('btnLogin');
-  loginButton.addEventListener('click', fValidation);
 
-  let resetButton = document.getElementById('btnReset');
-  resetButton.addEventListener('click', fReset);
+//Function for Load event
+function fLoad(){
+    //Get login button
+    let loginButton = document.getElementById('btnLogin');
+    loginButton.addEventListener('click', fValidation);
+
+    //Get reset button
+    let resetButton = document.getElementById('btnReset');
+    resetButton.addEventListener('click', fReset);
 }
+
+//Function for login btn click event and validation
 function fValidation(){
+    //Select all text input
     const inputs = document.querySelectorAll('input[type="text"]');
     let len = inputs.length;
     for(let i=0; i<len;i++){
+        //If value not provided set background color to red else green
         if (inputs[i].value.length === 0) {
             inputs[i].style.backgroundColor = 'red';
         } else {
@@ -19,10 +27,13 @@ function fValidation(){
     }
 }
 
+//Function for reset button click event
 function fReset(){
+    //Select all text input
     const inputs = document.querySelectorAll('input[type="text"]');
     let len = inputs.length;
     for(let i=0; i<len;i++){
+        //Reset background color and value
         inputs[i].style.backgroundColor = '';
         inputs[i].value = '';
     }
