24189ba563c99d821b63c4acd3c571727b570d32
gandharkulkarni <gakulkarni@dons.usfca.edu>
2023-07-11 17:05:53 -0700

    Lecture 18 examples

---
+++ Files/Examples/Example18-2.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", function() {
+	let btn =this.document.getElementById('btnRound')
+	//Register click event
+	btn.addEventListener('click',function(){
+		//Add class rounded border
+		document.getElementById('inner').classList.toggle('rounded-5');
+	})
+	//Closing curly brace, parenthesis		
+})
+
+
