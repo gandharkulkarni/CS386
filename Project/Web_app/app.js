530971b30f43d1d735a6ca08469db3afb78f2bb2
gandharkulkarni <gakulkarni@dons.usfca.edu>
2023-08-14 15:17:26 -0700

    added deployed code changes

--- Project/Web_app/app.js
+++ Project/Web_app/app.js
@@ -13,12 +13,12 @@ app.use(cors()); // Use cors middleware
 
 const routes = require('./routes/routes'); // Import the routes.js file
 app.use('/', routes); // Use the routes with '/' prefix
-// app.use(express.static(path.join(__dirname , 'react-app')));
+app.use(express.static(path.join(__dirname , 'react-app')));
 
 // For all other routes, serve the React app
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'react-app', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'react-app', 'index.html'));
+});
 
 module.exports = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
