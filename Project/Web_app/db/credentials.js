86d02ce11743d4193b60d32b6d3c21cbe6c42189
Gandhar Kulkarni <111133878+gandharkulkarni@users.noreply.github.com>
2023-08-19 02:40:20 -0700

    Update credentials.js

--- Project/Web_app/db/credentials.js
+++ Project/Web_app/db/credentials.js
@@ -1,5 +1,5 @@
 module.exports = {
 	mongo: {
-		connectionString: 'mongodb+srv://gakulkarni:cs386@fullstackwebdev.cscb82e.mongodb.net/USF_CS386_PROJECT?retryWrites=true&w=majority'
+		connectionString: ''
 	}
 };
